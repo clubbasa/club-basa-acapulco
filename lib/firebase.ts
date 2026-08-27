b1ddbab88d71b1e56f36074d6e175b2c62626ce0
clubbasa <96852787+clubbasa@users.noreply.github.com>
2026-08-26 20:53:52 -0600

    fix(v2): export Google auth provider

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,8 +1,10 @@
 'use client';
 
 import { getFirestore } from 'firebase/firestore';
+import { GoogleAuthProvider } from 'firebase/auth';
 import { firebaseApp } from '@/lib/firebase-config';
 export { auth } from '@/lib/firebase-auth';
 
 export { firebaseApp };
 export const db = getFirestore(firebaseApp);
+export const googleProvider = new GoogleAuthProvider();
